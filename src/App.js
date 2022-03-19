5e8ab1c789128beb1fb99bc973cfa4c0a40925f8
Muhammad Irfan <irfhan025@gmail.com>
2022-03-19 10:28:20 +0800

    initialize img carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import ImgCarousel from "./components/ImgCarousel";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ImgCarousel />
     </div>
   );
 }
---
+++ src/components/ImgCarousel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ImgCarousel() {
+  return <div>ImgCarousel</div>;
+}
+
+export default ImgCarousel;
